a5e27a9338268a78d929291419f60e933ddde32d
Alhadis <gardnerjohng@gmail.com>
2018-11-10 08:55:03 +1100

    Add logic for parsing character metrics

--- adobe-font-metrics.mjs
+++ adobe-font-metrics.mjs
@@ -1,13 +1,15 @@
 export default class AdobeFontMetrics{
 	
 	constructor(input = ""){
-		this.version    = "";
-		this.globalInfo = {};
-		this.directions = [{}, {}];
+		this.version     = "";
+		this.globalInfo  = {};
+		this.directions  = [{}, {}];
+		this.charMetrics = {};
 		Object.defineProperty(this, "parserState", {
 			enumerable: false,
 			value: {
 				direction: 0,
+				section: "header",
 				tail: "",
 			},
 		});
@@ -26,6 +28,19 @@ export default class AdobeFontMetrics{
 	}
 	
 	readLine(input){
+		switch(this.parserState.section){
+			case "header":
+				this.readHeaderField(input);
+				break;
+			case "chars":
+				const char = new CharacterMetric(input);
+				this.charMetrics[char.code] = char;
+				break;
+		}
+	}
+	
+	
+	readHeaderField(input){
 		if(input = input.match(/^(\S+)\s*(\S.*)?$/)){
 			const [, key, value] = input;
 			const lcKey = key[0].toLowerCase() + key.substr(1);
@@ -37,6 +52,10 @@ export default class AdobeFontMetrics{
 				
 				case "StartDirection":
 					this.parserState.direction = +value || 0;
+					break;
+				
+				case "StartCharMetrics":
+					this.parserState.section = "chars";
 					break;
 				
 				// Strings
@@ -74,11 +93,11 @@ export default class AdobeFontMetrics{
 				case "IsBaseFont":
 				case "IsCIDFont":
 				case "IsFixedV":
-					this.globalInfo[lcKey] = this.parseBoolean(value);
+					this.globalInfo[lcKey] = parseBoolean(value);
 					break;
 				
 				case "IsFixedPitch":
-					this.setDirectionProperty(lcKey, this.parseBoolean(value));
+					this.setDirectionProperty(lcKey, parseBoolean(value));
 					break;
 				
 				// Arrays
@@ -86,69 +105,20 @@ export default class AdobeFontMetrics{
 				case "BlendDesignPositions":
 				case "BlendDesignMap":
 				case "WeightVector":
-					this.globalInfo[lcKey] = this.parseArray(value)[0];
+					this.globalInfo[lcKey] = parseArray(value)[0];
 					break;
 				
 				case "VVector":
-					this.globalInfo[lcKey] = this.parseArray(value);
+					this.globalInfo[lcKey] = parseArray(value);
 					break;
 				
 				case "FontBBox":
-					this.globalInfo.boundingBox = this.parseArray(value);
+					this.globalInfo.boundingBox = parseArray(value);
 					break;
 			}
 		}
 	}
 	
-	parseArray(input){
-		let list = {parent: null, tokens: []};
-		let depth = 0;
-		let token = "";
-		
-		const {length} = input;
-		const endToken = () => {
-			if(token){
-				if("true" === token || "false" === token)
-					token = "true" === token;
-				else if("/" === token[0])
-					token = token.substr(1);
-				else
-					token = parseFloat(token);
-				list.tokens.push(token);
-			}
-			token = "";
-		};
-		for(let i = 0; i < length; ++i){
-			const char = input[i];
-			
-			// Begin new array
-			if("[" === char){
-				endToken();
-				list = {parent: list, tokens: []};
-				++depth;
-			}
-			
-			// Terminate current array
-			else if("]" === char && depth > 0){
-				endToken();
-				const {tokens} = list;
-				(list = list.parent).tokens.push(tokens);
-				--depth;
-			}
-			
-			// Terminate token upon whitespace
-			else if(/\s/.test(char))
-				endToken();
-			
-			else token += char;
-		}
-		return list.tokens;
-	}
-	
-	parseBoolean(input){
-		return "true" === input.trim().toLowerCase();
-	}
-
 	setDirectionProperty(key, value){
 		const dir = this.parserState.direction;
 		if(2 === dir)
@@ -157,4 +127,118 @@ export default class AdobeFontMetrics{
 		else
 			this.directions[dir][key] = value;
 	}
+}
+
+
+class CharacterMetric{
+	constructor(input){
+		this.code        = -1;
+		this.name        = "";
+		this.widths      = [0, 0];
+		this.heights     = [0, 0];
+		this.boundingBox = [0, 0, 0, 0];
+		this.ligatures   = {};
+		
+		for(const field of input.trim().split(/\s*;\s*/)){
+			const [key, ...values] = field.split(/\s+/);
+			switch(key){
+				case "C":
+				case "CH":
+					this.code = parseHex(values[0]);
+					break;
+				case "N":
+					this.name = values.join(" ");
+					break;
+				case "WX":
+				case "W0X":
+					this.widths[0] = +values[0] || 0;
+					break;
+				case "W1X":
+					this.widths[1] = +values[0] || 0;
+					break;
+				case "WY":
+				case "W0Y":
+					this.heights[0] = +values[0] || 0;
+					break;
+				case "W1Y":
+					this.heights[1] = +values[0] || 0;
+					break;
+				case "W":
+				case "W0":
+					this.widths[0]  = +values[0] || 0;
+					this.heights[0] = +values[1] || 0;
+					break;
+				case "W1":
+					this.widths[1]  = +values[0] || 0;
+					this.heights[1] = +values[1] || 0;
+					break;
+				case "VV":
+					this.widths[0]  = this.widths[1]  = +values[0] || 0;
+					this.heights[0] = this.heights[1] = +values[1] || 0;
+					break;
+				case "B":
+					this.boundingBox.splice(0, values.length, ...values.map(Number));
+					break;
+				case "L":
+					this.ligatures[values.shift()] = values.join(" ");
+					break;
+			}
+		}
+	}
+}
+
+
+function parseHex(input){
+	return "<" === input[0]
+		? parseInt(input.replace(/^<|>$/g, ""), 16)
+		: parseInt(input);
+}
+
+function parseArray(input){
+	let list = {parent: null, tokens: []};
+	let depth = 0;
+	let token = "";
+	
+	const {length} = input;
+	const endToken = () => {
+		if(token){
+			if("true" === token || "false" === token)
+				token = "true" === token;
+			else if("/" === token[0])
+				token = token.substr(1);
+			else
+				token = parseFloat(token);
+			list.tokens.push(token);
+		}
+		token = "";
+	};
+	for(let i = 0; i < length; ++i){
+		const char = input[i];
+		
+		// Begin new array
+		if("[" === char){
+			endToken();
+			list = {parent: list, tokens: []};
+			++depth;
+		}
+		
+		// Terminate current array
+		else if("]" === char && depth > 0){
+			endToken();
+			const {tokens} = list;
+			(list = list.parent).tokens.push(tokens);
+			--depth;
+		}
+		
+		// Terminate token upon whitespace
+		else if(/\s/.test(char))
+			endToken();
+		
+		else token += char;
+	}
+	return list.tokens;
+}
+
+function parseBoolean(input){
+	return "true" === input.trim().toLowerCase();
 }
